8e28f76cf7fbcc35a7ab97287df88b5fa51d73e8
Myeongjin (Daniel) <103289261+DanisLol@users.noreply.github.com>
2026-08-07 21:39:43 -0400

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+        result = [0] * len(temperatures)
+        temp = [] #contains (temperature, index)
+        for i, t in enumerate(temperatures):
+
+            while temp != [] and t > temp[-1][0]:
+                popped_i = temp.pop()[1]
+                result[popped_i] = i - popped_i
+
+            temp.append((t,i))
+        
+        return result
+
