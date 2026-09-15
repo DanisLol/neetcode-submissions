e903a907594d11a39876159d4cc8d20e44d5a2c0
Myeongjin (Daniel) <103289261+DanisLol@users.noreply.github.com>
2026-09-15 15:34:40 -0400

    Add: reorder-linked-list - submission-2

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-2.py
@@ -0,0 +1,29 @@
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        #last node .next should be the second node
+        #store the second node in a set
+        #have the last node.next = second node from the set
+        
+        #slow and fast pointer to find middle element
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        prev = slow.next = None
+
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
+        
